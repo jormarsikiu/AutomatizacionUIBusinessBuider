0cf5827b0aecc687ad8bbdb7c96299b7e1441676
jormar.turizo <jormar.turizo@eurokaizen.com>
2022-12-12 11:26:45 -0400

    12/12/2022 11:16am

--- UI_test/step_defs/test.py
+++
@@ -1,9 +0,0 @@
-import random
-
-GroupCode = str(random.randint(0,10000))
-
-string1 = '''document.querySelectorAll("[href*='''
-string2 = "'/Settings"+"/EntityGroup/EditGroupFormAsync?entityCode="+GroupCode+"'"
-string3 = ''']")[0].click()'''
-string4 = string1+string2+string3
-print(string4)
